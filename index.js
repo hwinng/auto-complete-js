2e2a2447b70bbd365858d89abca25a30b7f8b463
Phillip Nguyen <macintoshhd@phillip.lan>
2022-08-28 16:01:04 +0700

    done:

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
 const apis = Object.freeze({
-    query_with_shirt: "https://api.json-generator.com/templates/k78yyghl9qrn/data",
+    query_by_sh: "https://api.json-generator.com/templates/k78yyghl9qrn/data",
+    query_by_shirt: "https://api.json-generator.com/templates/PGMv7BZg8RT8/data",
+    query_by_polo: "https://api.json-generator.com/templates/djN4XlWG0hHg/data",
 });
+
+const TOKEN = 'ydckmbhofs2oqlsn1aap2n8wv1txvhnt4vume9wa';
+
+const ORDER_SETTING = Object.freeze({
+    SUGGESTION: "SUGGESTION",
+    COLLECTION: "COLLECTION",
+    PRODUCT: "PRODUCT"
+})
 
 class DOMClass {
     createElement(tag, className, id) {
@@ -14,24 +25,45 @@ class DOMClass {
         const element = document.querySelector(selector);
         return element;
     }
+
+    styleBlock($node, style) {
+        $($node).css(style);
+    }
 }
 
 class WidgetModel {
-    constructor() {
+    constructor(numberOfCharacter = 1) {
         this.data = null;
+        this.numberOfCharacter = numberOfCharacter;
     }
 
+    // fixed api call depends on numberOfCharacter
     getSearchResult(keyword) {
         return new Promise((resolve, reject) => {
             let keyStr = keyword.toString().toLowerCase();
-            if (keyStr.length > 0 && 'shirt'.includes(keyStr)) {
-                this.fetchData(apis.query_with_shirt)
+            if (keyStr === 's' || keyStr === 'sh') {
+                this.fetchData(apis.query_by_sh)
                     .then((res) => {
                         this.data = res;
                         resolve(this.data);
                     })
                     .catch(_ => reject(_));
-            } else {
+            } else if (keyStr.length > 2 && 'shirt'.includes(keyStr)) {
+                this.fetchData(apis.query_by_shirt)
+                    .then((res) => {
+                        this.data = res;
+                        resolve(this.data);
+                    })
+                    .catch(_ => reject(_));
+            } else if ('polo'.includes(keyStr)) {
+                this.fetchData(apis.query_by_polo)
+                    .then((res) => {
+                        this.data = res;
+                        resolve(this.data);
+                    })
+                    .catch(_ => reject(_));
+            }
+            else {
                 resolve([]);
             }
         })
@@ -42,7 +74,7 @@ class WidgetModel {
             $.ajax({
                 type,
                 url,
-                headers: { "Authorization": "Bearer ydckmbhofs2oqlsn1aap2n8wv1txvhnt4vume9wa" },
+                headers: { "Authorization": `Bearer ${TOKEN}` },
                 dataType: 'json',
                 contentType: 'application/json; charset=utf-8',
                 success: function (result) {
@@ -57,13 +89,19 @@ class WidgetModel {
 }
 
 class WidgetView extends DOMClass {
-    // dynamic DOM query for input fields
-    constructor(inputNodeId, clearInputId) {
+    constructor(inputNodeId, clearInputId, widgetNodeId, suggestionSetting, collectionSetting, productSetting, orderSetting) {
         super();
-        this.inputNodeId = inputNodeId;
-        this.clearInputId = clearInputId;
         this.createElement = super.createElement;
         this.getElement = super.getElement;
+        this.styleBlock = super.styleBlock;
+
+        this.inputNodeId = inputNodeId;
+        this.clearInputId = clearInputId;
+        this.widgetNodeId = widgetNodeId;
+        this.suggestionSetting = suggestionSetting;
+        this.collectionSetting = collectionSetting;
+        this.productSetting = productSetting;
+        this.orderSetting = orderSetting;
 
         this.$input = this.getElement(`#${this.inputNodeId}`);
         this.$clearBtn = this.getElement(`#${this.clearInputId}`);
@@ -71,7 +109,7 @@ class WidgetView extends DOMClass {
 
         this.$popupContainer = this.createElement('div', 'popup-container', 'popup-container');
         this.$popoverBtn = this.createElement('div', 'search-popover');
-        this.$widgetContainer = this.createElement('div', 'widget-container', 'widget-container');
+        this.$widgetContainer = this.createElement('div', this.widgetNodeId, this.widgetNodeId);
 
         // add popover btn and suggestion container div into popupContainer
         this.$popupContainer.appendChild(this.$popoverBtn);
@@ -82,7 +120,7 @@ class WidgetView extends DOMClass {
         this.toggleUIState();
     }
 
-    displayWidget(data, cb) {
+    displayWidget(data) {
         this.toggleUIState();
 
         // remove all child nodes of widgetContainer
@@ -97,13 +135,20 @@ class WidgetView extends DOMClass {
             this.$widgetContainer.appendChild(p);
         } else {
             const { terms, collections, products } = data[0];
-            // handle display suggestion herer
-            const suggestionsBlock = new SuggestionsBlock('Suggestions', 4, this.$widgetContainer, this.$input, 'term-container',);
-            const collectionsBlock = new CollectionsBlock('Collections', 4, this.$widgetContainer, this.$input, 'collection-container');
-            const productsBlock = new ProductsBlock('Products', 3, this.$widgetContainer, this.$input, 'product-container');
-            suggestionsBlock.displayBlock(terms);
-            collectionsBlock.displayBlock(collections);
-            productsBlock.displayBlock(products);
+            for (let i = 0; i < this.orderSetting.length; i++) {
+                if (this.orderSetting[i] === ORDER_SETTING.SUGGESTION) {
+                    const suggestionView = new SuggestionsBlock(this.suggestionSetting.title, this.suggestionSetting.maximumItems, this.$widgetContainer, this.$input, this.suggestionSetting.className);
+                    suggestionView.displayBlock(terms);
+                }
+                if (this.orderSetting[i] === ORDER_SETTING.COLLECTION) {
+                    const collectionView = new CollectionsBlock(this.collectionSetting.title, this.collectionSetting.maximumItems, this.$widgetContainer, this.$input, this.collectionSetting.className);
+                    collectionView.displayBlock(collections);
+                }
+                if (this.orderSetting[i] === ORDER_SETTING.PRODUCT) {
+                    const productView = new ProductsBlock(this.productSetting.title, this.productSetting.maximumItems, this.$widgetContainer, this.$input, this.productSetting.className);
+                    productView.displayBlock(products);
+                }
+            }
 
         }
 
@@ -117,11 +162,11 @@ class WidgetView extends DOMClass {
 
     toggleUIState() {
         if (!this.$input?.value) {
-            this.$clearBtn.style.display = 'none';
-            this.$popupContainer.style.display = 'none';
+            this.styleBlock(this.$clearBtn, { display: 'none' });
+            this.styleBlock(this.$popupContainer, { display: 'none' });
         } else {
-            this.$clearBtn.style.display = 'block';
-            this.$popupContainer.style.display = 'block';
+            this.styleBlock(this.$clearBtn, { display: 'block' });
+            this.styleBlock(this.$popupContainer, { display: 'block' });
         }
     }
 }
@@ -173,7 +218,6 @@ class WidgetBlock extends DOMClass {
                 $ul.appendChild($li);
             })
         };
-
         this.$blockNode.appendChild($ul);
     };
 }
@@ -196,19 +240,22 @@ class ProductsBlock extends WidgetBlock {
     }
 
     // Override from WidgetBlock
-    displayBlock(products) {
+    displayBlock(products,) {
         let $ul = this.createElement('ul', 'product-list');
         if (products.length) {
             products.slice(0, this.maximumItems).forEach((product) => {
                 let $li = this.createElement('li', 'item');
                 let $a = this.initProductItemNode(product);
-
                 $li.appendChild($a);
                 $ul.appendChild($li);
             })
         };
 
+        const $productLink = this.generateProductLinks(products.length, '/products');
         this.$blockNode.appendChild($ul);
+        this.$blockNode.appendChild($productLink);
+
+
     }
 
     initProductItemNode(product) {
@@ -241,6 +288,13 @@ class ProductsBlock extends WidgetBlock {
 
         return $a;
     }
+
+    generateProductLinks(numberOfProducts, url) {
+        let $productLink = this.createElement('a', 'product-links');
+        $productLink.href = url;
+        $productLink.textContent = `View all ${numberOfProducts} products`;
+        return $productLink;
+    }
 }
 
 class WidgetController {
@@ -259,14 +313,82 @@ class WidgetController {
     }
 }
 
-
 // TEST
-const app = new WidgetController(new WidgetModel(), new WidgetView('search', 'clear-search-btn'));
+function main() {
+    const orderSetting = [ORDER_SETTING.SUGGESTION, ORDER_SETTING.COLLECTION, ORDER_SETTING.PRODUCT];
+    const suggestionSetting = {
+        title: 'Suggestions',
+        maximumItems: 4,
+        className: 'term-container'
+    }
+    const collectionSetting = {
+        title: 'Collections',
+        maximumItems: 2,
+        className: 'collection-container'
+    }
+    const productSetting = {
+        title: 'Products',
+        maximumItems: 6,
+        className: 'product-container'
+    }
 
-$(`#${app.view.inputNodeId}`).keyup(function (e) {
-    app.onChangeHandler(e.target.value);
-});
+    const widgetView = new WidgetView('search', 'clear-search-btn', 'widget-container', suggestionSetting, collectionSetting, productSetting, orderSetting);
+    const widgetModel = new WidgetModel();
+    const app = new WidgetController(widgetModel, widgetView);
+    const appView = app.view;
+    const appModel = app.model;
 
-$(`#${app.view.clearInputId}`).click(function () {
-    app.view.clearSearch();
-})
+    // re-render widget base on number of setting characters
+    $(`#${appView.inputNodeId}`).keyup(function (e) {
+        if (e.target.value.length === 0) {
+            appView.toggleUIState();
+        } else {
+            let { numberOfCharacter } = appModel;
+            // each keyup event fires will re-render once
+            if (numberOfCharacter === 1) {
+                app.onChangeHandler(e.target.value);
+            }
+            // each numberOfCharacter will re-render again
+            if (appModel.numberOfCharacter > 1) {
+                let inputVal = appView.$input?.value;
+                if (inputVal.length && inputVal.length % Number(numberOfCharacter) === 0) {
+                    app.onChangeHandler(e.target.value);
+                }
+            }
+        }
+    });
+
+    $(`#${appView.clearInputId}`).click(function () {
+        appView.clearSearch();
+    })
+
+    // use event delegation to handle multiple same clicks on each label of block
+    $(`#${appView.widgetNodeId}`).click(function (e) {
+        if (e.target.tagName === 'P' && e.target.className === 'label') {
+            const $nextSibling = e.target.nextSibling;
+            let style = {
+                display: 'none'
+            };
+            if ($nextSibling.style.display === 'none') {
+                if ($nextSibling.className === 'product-list') {
+                    style = {
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingLeft: '12px',
+                        marginBlock: 0,
+                    }
+                    appView.styleBlock($nextSibling, style);
+                } else {
+                    style = {
+                        display: 'block'
+                    }
+                    appView.styleBlock($nextSibling, style);
+                }
+            } else {
+                appView.styleBlock($nextSibling, style);
+            }
+        }
+    });
+}
+
+main();
